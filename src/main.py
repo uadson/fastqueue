aa12abc4638c6a7788ce21f2dee8942840d8604d
Uadson Feitosa <uadsonpy@gmail.com>
2025-03-11 16:25:04 -0300

    refeactor: update path from import in main module

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ Rotas disponíveis:
 
 from fastapi import FastAPI
 
-from celery_worker import fetch_data
+from .celery_worker import fetch_data
 
 # Inicializa a aplicação FastAPI
 app = FastAPI()
 
 
-@app.post('/request/')
+@app.post("/request/")
 def send_request(url: str, token: str):
     """
     Enfileira uma requisição para uma API externa.
@@ -27,12 +27,12 @@ def send_request(url: str, token: str):
             dict: ID da tarefa Celery e status inicial.
     """
 
-    headers = {'Authorization': f'Bearer {token}'}
-    task = fetch_data.apply_async(args=[url], kwargs={'headers': headers})
-    return {'task_id': task.id, 'status': task.status}
+    headers = {"Authorization": f"Bearer {token}"}
+    task = fetch_data.apply_async(args=[url], kwargs={"headers": headers})
+    return {"task_id": task.id, "status": task.status}
 
 
-@app.get('/status/{task_id}')
+@app.get("/status/{task_id}")
 def get_task_status(task_id: str):
     """
     Retorna o status da tarefa enfileirada.
@@ -46,7 +46,7 @@ def get_task_status(task_id: str):
 
     task_result = fetch_data.AsyncResult(task_id)
     return {
-        'task_id': task_id,
-        'status': task_result.status,
-        'result': task_result.result if task_result.ready() else None,
+        "task_id": task_id,
+        "status": task_result.status,
+        "result": task_result.result if task_result.ready() else None,
     }
